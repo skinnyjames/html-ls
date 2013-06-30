bd0023bd216313a7ff8f51f60a9c70e567996642
jesse keane <me@jessekeane.me>
2013-07-01 00:06:41 +0400

    tests passing

--- test/test.js
+++ test/test.js
@@ -1,3 +1,40 @@
-var assert = require('assert');
+var assert = require('assert'),
+    htmlls = require('../index.js'),
+    fs = require('fs');
 
-assert.ok(true);
+fs.mkdirSync('test-htmlls');
+fs.mkdirSync('test-htmlls/dir');
+fs.writeFileSync('test-htmlls/dir/file.exe', 'blah');
+fs.writeFileSync('test-htmlls/file.txt', 'wee');
+
+var step = 0,
+    stream = htmlls(process.cwd() + '/test-htmlls');
+
+stream.on('data', function (data) {
+  if (data != null && typeof data != 'string') { data = data.toString(); }
+  switch (step) {
+    case 0:
+      assert.equal(data, '<ul>\n');
+      break;
+    case 1:
+      assert.ok(data.match(/dir\//));
+      break;
+    case 2:
+      assert.ok(data.match(/file.txt/));
+      break;
+    case 3:
+      assert.equal(data, '</ul>\n');
+      break;
+    case 4:
+      assert.equal(data, null);
+      tearDown();
+      break;
+  }
+  step++;
+});
+function tearDown() {
+  fs.unlinkSync('test-htmlls/file.txt');
+  fs.unlinkSync('test-htmlls/dir/file.exe');
+  fs.rmdirSync('test-htmlls/dir');
+  fs.rmdirSync('test-htmlls');
+}
