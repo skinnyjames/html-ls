3e0a51b60fb0cedef8c64f0d31cd99bb2cbddc95
jesse keane <me@jessekeane.me>
2013-06-30 23:42:47 +0400

    first pass

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var ls = require('ls-stream');
+var ls = require('ls-stream'),
+    path = require('path'),
+    through = require('through');
 
+function lsHtml() {
+  return function (dir) {
+    var stream = through();
 
+    process.nextTick(go);
+    return stream;
+
+    function go() {
+    stream.queue('<ul>\n');
+    ls(dir)
+      .on('data', function (data) {
+        if (data == null) {
+          done();
+          return;
+        }
+        var objectName = path.basename(data.path),
+            objectString = '<li><a href="' + objectName + '">' + objectName + (data.isDirectory() ? '/' : '') + '</a></li>\n';
+        stream.queue(objectString);
+      });
+    }
+    function done() {
+      stream.queue('</ul>');
+      stream.queue(null);
+    }
+  }
+}
+
+module.exports = lsHtml;
