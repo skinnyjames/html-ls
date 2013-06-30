4b8116a186c8416676355cdbb270b39d838c3470
jesse keane <me@jessekeane.me>
2013-06-30 23:51:19 +0400

    first working

--- index.js
+++ index.js
@@ -2,30 +2,29 @@ var ls = require('ls-stream'),
     path = require('path'),
     through = require('through');
 
-function lsHtml() {
-  return function (dir) {
-    var stream = through();
+function lsHtml(dir) {
+  var stream = through();
 
-    process.nextTick(go);
-    return stream;
+  process.nextTick(go);
+  return stream;
 
-    function go() {
-    stream.queue('<ul>\n');
-    ls(dir)
-      .on('data', function (data) {
-        if (data == null) {
-          done();
-          return;
-        }
-        var objectName = path.basename(data.path),
-            objectString = '<li><a href="' + objectName + '">' + objectName + (data.isDirectory() ? '/' : '') + '</a></li>\n';
-        stream.queue(objectString);
-      });
-    }
-    function done() {
-      stream.queue('</ul>');
-      stream.queue(null);
-    }
+  function go() {
+  stream.queue('<ul>\n');
+  ls(dir)
+    .on('data', function (data) {
+      // if we receive a null or if we have recursed into a new dir, just terminate the stream
+      if (data == null || path.dirname(data.path) !== dir) {
+        done();
+        return;
+      }
+      var objectName = path.basename(data.path),
+          objectString = '<li><a href="' + objectName + '">' + objectName + (data.stat.isDirectory() ? '/' : '') + '</a></li>\n';
+      stream.queue(objectString);
+    });
+  }
+  function done() {
+    stream.queue('</ul>\n');
+    stream.queue(null);
   }
 }
 
