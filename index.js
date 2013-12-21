d0c8827c7487525b52c10f7abf61b83eca2d39f9
jesse keane <me@jessekeane.me>
2013-12-20 17:55:35 -0800

    cleanup

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 var ls = require('ls-stream'),
     path = require('path'),
-    through = require('through');
+    through = require('through')
+
+module.exports = lsHtml
 
 function lsHtml(dir, options) {
-
-  options = options || {};
-
   var hideDot = options.hideDot,
       showUp = options.showUp,
-      stream = through();
+      stream = through()
 
-  process.nextTick(go);
+  options = options || {}
+
+  setImmediate(go)
   
-  return stream;
+  return stream
 
   function go() {
-    stream.queue('<ul>\n');
+    stream.queue('<ul>\n')
     if (showUp) {
-      var upDir = path.dirname(dir) + '/';
-      stream.queue('<li><a href="' + upDir + '">..</a></li>\n');
+      var upDir = path.dirname(dir) + '/'
+      stream.queue('<li><a href="' + upDir + '">..</a></li>\n')
     }
     ls(dir)
       .on('data', function (data) {
-        // if we receive a null or if we have recursed into a new dir, just terminate the stream
-        if (data == null || path.dirname(data.path) !== dir) return done();
-        var objectName = path.basename(data.path) + (data.stat.isDirectory() ? '/' : ''),
-            objectString = ['<li><a href="', objectName, '">', objectName, '</a></li>\n'].join('');
-        if (hideDot && /^\./.test(objectName)) return;
-        stream.queue(objectString);
+        if (data === null || path.dirname(data.path) !== dir) return done()
+        var objectName = path.basename(data.path) +
+              (data.stat.isDirectory() ? '/' : ''),
+            objectString = [
+              '<li><a href="',
+              objectName,
+              '">',
+              objectName,
+              '</a></li>\n'].join('')
+
+        if (hideDot && /^\./.test(objectName)) return
+
+        stream.queue(objectString)
       })
-      .on('end', done);
+      .on('end', done)
   }
   function done() {
-    stream.queue('</ul>\n');
-    stream.queue(null);
+    stream.queue('</ul>\n')
+    stream.queue(null)
   }
 }
-
-module.exports = lsHtml;
-
